c1bb365567d56475d21742d00102db79d766aafc
yassermej <yassermej50@gmail.com>
2018-02-09 20:07:49 -0800

    test: manager of campaign contract is address that deployed it

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -38,4 +38,8 @@ describe('Campaigns', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+  it('marks deployer as campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
 });
