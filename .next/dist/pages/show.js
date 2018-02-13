dd5363b38dd95be87f0328e66df8d323f3903b87
yassermej <yassermej50@gmail.com>
2018-02-12 18:13:55 -0800

    chore: install next, react pages dir

---
+++ .next/dist/pages/show.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/home/donquixote/PROJECTS/BlockchainApps/ether-fund-react/pages/show.js?entry';
+
+exports.default = function () {
+  return _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, ' TEST SHOW PAGE ');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL3Nob3cuanMiXSwibmFtZXMiOlsiUmVhY3QiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU8sQUFFUDs7Ozs7Ozs7a0JBQWUsWUFBTSxBQUNuQjt5QkFBTyxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsR0FBQSxFQUFQLEFBQU8sQUFDUjtBQUZEIiwiZmlsZSI6InNob3cuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL2hvbWUvZG9ucXVpeG90ZS9QUk9KRUNUUy9CbG9ja2NoYWluQXBwcy9ldGhlci1mdW5kLXJlYWN0In0=
---
+++ pages/newcampaign.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <h1> TEST PAGE </h1>;
+};
